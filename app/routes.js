6db072d1647352d81ce24ecb6f5c50aab9c890fd
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2025-02-24 19:59:26 +0600

    removed remix template and reconvigured vite+react+tailwind+react-router(hash)

--- app/routes.js
+++
@@ -1,6 +0,0 @@
-import { index } from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
-
-export default flatRoutes({
-    rootDirectory: "routes",
-  });
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <Routes>
+      <Route path="/" element={<>rafiz index</> } />
+      <Route path="/test" element={<>fiz test <p className='text-red-500'>this is styled</p></> } />
+    </Routes>
+    </>
+  )
+}
+
+export default App
